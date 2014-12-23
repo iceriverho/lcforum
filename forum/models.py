58bd8baa87cae9acbeebe466c9a9e97b30dacc62
lichifeng <lichifeng@qq.com>
2014-12-23 21:59:03 +0800

    improved form style

--- forum/models.py
+++ forum/models.py
@@ -17,17 +17,18 @@ class DateTimeBase(models.Model):
 
 class PostBase(DateTimeBase):
     title = models.CharField(max_length=200, error_messages={
+        'blank': u"标题不能为空",
         'null': u"标题不能为空",
         'invalid': u"您输入了一个无效的标题，标题的长度请控制在100个字符内"
-    }, help_text=u"标题为必填且不能超过100个字符",
+    }, help_text=u"☞标题为必填且不能超过100个字符",
                              verbose_name=u"标题")
     content = models.TextField(blank=True, null=True,
-                               help_text=u"正文可以留空，但不要输入无意义的内容",
+                               help_text=u"☞正文可以留空，但不要输入无意义的内容",
                                verbose_name=u"正文")
     author = models.ForeignKey(User, blank=True, null=True, related_name='%(class)s', on_delete=models.SET_NULL)
     content_md = models.TextField(blank=True, null=True)
     bygod = models.BooleanField(blank=True, default=False,
-                                help_text=u"将这篇文章归入管理员文集",
+                                help_text=u"☞将这篇文章归入管理员文集",
                                 verbose_name=u"归档")
 
     def save(self, *args, **kwargs):
@@ -41,12 +42,13 @@ class PostBase(DateTimeBase):
 
 class NodeTag(DateTimeBase):
     name = models.CharField(max_length=50, error_messages={
+        'blank': u"节点的名称不能为空",
         'null': u"节点的名称不能为空",
         'invalid': u"您输入了一个无效的节点名称，节点名称的长度不能超过50个字符"
-    }, help_text=u"节点名称为必填且不能超过50个字符",
+    }, help_text=u"☞节点名称为必填且不能超过50个字符",
                             verbose_name=u"节点名称")
     description = models.TextField(blank=True, null=True,
-                                   help_text=u"关于节点讨论主题的简要描述",
+                                   help_text=u"☞关于节点讨论主题的简要描述",
                                    verbose_name=u"节点描述")
 
     def get_absolute_url(self):
