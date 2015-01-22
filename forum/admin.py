20810a635d4a1ccc1d9191de3512f301fc79a6e3
lichifeng <lichifeng@qq.com>
2015-01-22 12:38:28 +0800

    add attachment to admin

--- forum/admin.py
+++ forum/admin.py
@@ -2,10 +2,11 @@
 
 from django.contrib import admin
 
-from .models import Post, Reply, NodeTag
+from .models import Post, Reply, NodeTag, Attachment
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Reply)
 admin.site.register(NodeTag)
+admin.site.register(Attachment)
